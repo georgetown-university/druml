da161d1a8227e4d4aab0600ad651864e7c01bdf0
Vadim Mirgorod <dealancer@gmail.com>
2014-10-14 21:46:43 -0400

    Updated remote-bash script to use new parameter parser

--- dmul-remote-bash.sh
+++ dmul-remote-bash.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-if [ $# -lt 4 ]
+if [ $# -lt 3 ]
 then
   echo "This command performs arbitrary drush commands for a specific subsite."
   echo ""
-  echo "Syntax: $0 <config> <subsite> <environment> <command 1> ... <command n>"
+  echo "Syntax: dmul remote-bash [--config=dmul.yml] [--list=list | --site=subsite] <environment> <command 1> ... <command n>"
   echo "You can use following variables in a command:"
-  echo " @SUBSITE - subsite machine name"
   echo " @DOCROOT - subsite docroot"
   echo " @LOG     - logs dir"
   exit 1
@@ -19,25 +18,27 @@ SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
 source $SCRIPT_DIR/dmul-inc-init.sh
 
 # Read parameters.
-SUBSITE=$2
-ENV=$(get_environment $3)
+ENV=$(get_environment ${ARG[1]})
 SSH_ARGS=$(get_ssh_args $ENV)
 DRUSH_ALIAS=$(get_drush_alias $ENV)
-shift && shift && shift
 
 # Read variables and form commands to execute.
-echo "=== Execute bash commands for '$SUBSITE' subsite on the $ENV environment"
+echo "=== Execute bash commands on the $ENV environment"
 echo "Commands to be executed:"
 
+# Read commands.
 COMMANDS=""
-while test ${#} -gt 0
+I=1
+for CMD in ${ARG[@]}
 do
-  COMMANDS="$COMMANDS $1"
-  shift
+  if [[ $I -gt 1 && -n ${ARG[$I]} ]]
+  then
+    COMMANDS="$COMMANDS ${ARG[$I]};"
+  fi
+  I=$((I+1))
 done
 
 # Replace variables.
-COMMANDS=${COMMANDS/@SUBSITE/$SUBSITE}
 COMMANDS=${COMMANDS/@DOCROOT/$(get_remote_docroot $ENV)}
 COMMANDS=${COMMANDS/@LOG/$(get_remote_log $ENV)}
 
@@ -45,7 +46,7 @@ COMMANDS=${COMMANDS/@LOG/$(get_remote_log $ENV)}
 echo $COMMANDS
 echo ""
 
-# Execute drush commands
+# Execute bash commands
 ssh -tn $SSH_ARGS "$COMMANDS"
 
 echo ""
