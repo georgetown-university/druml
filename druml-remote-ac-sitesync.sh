4f907cfea954cc8b4147411d0408c931ee2e8f73
Vadym Myrgorod <dealancer@gmail.com>
2016-07-08 17:53:10 -0400

    Back up target DB

--- druml-remote-ac-sitesync.sh
+++ druml-remote-ac-sitesync.sh
@@ -10,8 +10,8 @@ source $SCRIPT_DIR/druml-inc-init.sh
 if [[ ${#ARG[@]} -lt 2 || -z $PARAM_SITE || -n $PARAM_HELP ]]
 then
   echo "usage: druml remote-ac-sitesync [--config=<path>] [--delay=<seconds>]"
-  echo "                                [--site=<subsite> | --list=<list>]"
-  echo "                                <environment from> <environment to>"
+  echo "                               [--site=<subsite> | --list=<list>]"
+  echo "                               <environment from> <environment to>"
   exit 1
 fi
 
@@ -20,6 +20,14 @@ source $SCRIPT_DIR/druml-inc-config.sh
 
 SUBSITE=$PARAM_SITE
 ENV_TO=$(get_environment ${ARG[2]})
+
+# Backup target db.
+OUTPUT=$(run_script remote-ac-dbbackup --site=$SUBSITE $ENV_TO 2>&1)
+RESULT="$?"
+echo "$OUTPUT"
+if [[ $RESULT > 0 ]]; then
+  exit 1
+fi
 
 # Copy files.
 OUTPUT=$(run_script remote-filesync $PROXY_PARAMS $PROXY_ARGS 2>&1)
@@ -55,3 +63,5 @@ echo "$OUTPUT"
 if [[ $RESULT > 0 ]]; then
   exit 1
 fi
+
+# TODO: Flush Varnish cache.
