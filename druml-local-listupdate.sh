22a13d6fcff1038c20ea1ff04ef7b045123f3451
Vadym Myrgorod <vm386@georgetown.edu>
2016-08-08 16:10:33 -0400

    Update listupdate script

---
+++ druml-local-listupdate.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Save script dir.
+SCRIPT_DIR=$(cd $(dirname "$0") && pwd -P)
+
+# Load includes.
+source $SCRIPT_DIR/druml-inc-init.sh
+
+# Display help.
+if [[ -z $PARAM_LIST || -n $PARAM_HELP ]]
+then
+  echo "usage: druml local-listupdate [--config=<path>] [--docroot=<path>] --list=<list>"
+  exit 1
+fi
+
+# Read parameters.
+LISTFILE=$(get_list_file $PARAM_LIST)
+DOCROOT=$(get_docroot)
+
+if [[ -z $LISTFILE ]]
+then
+  echo "List file is not set!"
+  exit
+fi
+
+# Prepare list file.
+touch $LISTFILE; rm $LISTFILE; touch $LISTFILE
+
+# Add records to the list file.
+cd $DOCROOT/sites/
+for i in $(ls -d */);
+do
+  SUBSITE=${i%%/}
+
+  # Prevent from all sites directory and acquia dev desktop directories being otuputed.
+  if [[ "$SUBSITE" != "all" &&  "$SUBSITE" != *".dd"* ]]
+  then
+    echo $SUBSITE >> $LISTFILE
+  fi
+done
