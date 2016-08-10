2fef37048ed0d303d0610f0d92f2ad7409307d18
Vadym Myrgorod <vm386@georgetown.edu>
2016-08-10 17:06:57 -0400

    Remove unsued comments

--- druml-inc-init.sh
+++ druml-inc-init.sh
@@ -5,6 +5,3 @@ source $SCRIPT_DIR/druml-inc-yaml.sh
 source $SCRIPT_DIR/druml-inc-utils.sh
 source $SCRIPT_DIR/druml-inc-params.sh
 source $SCRIPT_DIR/druml-inc-config.sh
-
-# Make sure results are outputted immideately.
-# exec 2>&1
