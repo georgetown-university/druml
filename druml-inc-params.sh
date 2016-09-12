0a72832022dad21de61f56c645aa15e7d719697c
Vadym Myrgorod <vm386@georgetown.edu>
2016-09-12 15:00:05 -0400

    Fix parameters that use dash, make sure parameters with no value has value as their name is

--- druml-inc-params.sh
+++ druml-inc-params.sh
@@ -5,14 +5,14 @@ I=1
 while test ${#} -gt 0
 do
   _P_NAME=$(get_parameter_name $1)
-  _P_NAME_U=$(echo $_P_NAME | tr '[:lower:]' '[:upper:]')
+  _P_NAME_U=$(echo $_P_NAME | tr '[:lower:]' '[:upper:]' | tr '-' '_')
   _P_VALUE=$(get_parameter_value $1)
 
   if [[ -n $_P_NAME ]]
   then
     if [[ -z $_P_VALUE ]]
     then
-      _P_VALUE=_P_NAME_U
+      _P_VALUE=$_P_NAME_U
     fi
     _PARAM_NAME="PARAM_${_P_NAME_U}"
     eval $_PARAM_NAME=$_P_VALUE
