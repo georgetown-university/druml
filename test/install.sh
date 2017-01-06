ff2fdf13633418d5e96465c06c21b15d397a5974
Vadym Myrgorod <vm386@georgetown.edu>
2017-01-06 13:03:08 -0500

    Experiment with ssh key

--- test/install.sh
+++ test/install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo $SSH_KEY
 echo $SSH_KEY > ~/.ssh/id_rsa
-ssh -Tn drupal7druml.test@free-6255.devcloud.hosting.acquia.com "echo 123" 2>&1
+
+ssh -Tn drupal7druml.test@free-6255.devcloud.hosting.acquia.com "echo 123"
+echo "!"
 git clone https://github.com/sstephenson/bats.git
