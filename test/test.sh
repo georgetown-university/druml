434b8ea7f309b52775d2fd88907901d41d7ee0b7
Vadym Myrgorod <vm386@georgetown.edu>
2017-01-06 13:48:20 -0500

    playing with ssh keys

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+
+echo "!"
+ssh -Tn drupal7druml.test@free-6255.devcloud.hosting.acquia.com "echo 123"
+echo "!"
+git clone https://github.com/sstephenson/bats.git
+
+
 cd test
+
 ../bats/bin/bats test.bats
