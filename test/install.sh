431bb48d47a6ac645167d3e1f14b07580e45c412
Vadym Myrgorod <vm386@georgetown.edu>
2017-01-06 14:28:52 -0500

    playing with ssh keys

--- test/install.sh
+++ test/install.sh
@@ -10,4 +10,8 @@ sudo chmod a-w $HOME/.ssh/id_rsa.pub
 sudo chmod go-r  $HOME/.ssh/id_rsa.pub
 
 eval `ssh-agent -s`
-ssh-add ~/.ssh/id_rsa
+echo "exec cat" > ap-cat.sh
+chmod a+x ap-cat.sh
+export DISPLAY=1
+echo "" | SSH_ASKPASS=./ap-cat.sh ssh-add ~/.ssh/id_rsa
+rm ap-cat.sh
