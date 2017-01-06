dcc6991f841f1509ee93e5b19e6f6763fd859485
Vadym Myrgorod <vm386@georgetown.edu>
2017-01-06 14:11:13 -0500

    playing with ssh keys

--- test/install.sh
+++ test/install.sh
@@ -8,4 +8,6 @@ sudo chmod a-w $HOME/.ssh/id_rsa
 sudo chmod go-r $HOME/.ssh/id_rsa
 sudo chmod a-w $HOME/.ssh/id_rsa.pub
 sudo chmod go-r  $HOME/.ssh/id_rsa.pub
+
+eval `ssh-agent -s`
 ssh-add ~/.ssh/travis/id_rsa
