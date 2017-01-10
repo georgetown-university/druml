aed7b583e028a2e03c67e3b08108e96697e05e6d
Vadym Myrgorod <vm386@georgetown.edu>
2017-01-10 14:38:29 -0500

    playing with ssh keys

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,9 @@
 eval `ssh-agent`
 ssh-add $HOME/.ssh/id_rsa
 
+cat $HOME/.ssh/id_rsa
+cat $HOME/.ssh/id_rsa.pub
+
 ssh -Tn drupal7druml.test@free-6255.devcloud.hosting.acquia.com "echo 123"
 
 cd test
