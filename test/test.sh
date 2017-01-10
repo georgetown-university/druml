0d9336b27829d1ff59b684e2daff107b6625f854
Vadym Myrgorod <vm386@georgetown.edu>
2017-01-10 14:55:31 -0500

    playing with ssh keys

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,19 @@
+
+printf "$SSH_KEY"
+printf "$SSH_KEY_PUB"
+
+
+printf "$SSH_KEY" > $HOME/.ssh/id_rsa
+printf "$SSH_KEY_PUB" > $HOME/.ssh/id_rsa.pub
+
 cat $HOME/.ssh/id_rsa
 cat $HOME/.ssh/id_rsa.pub
+
+
+sudo chmod a-w $HOME/.ssh/id_rsa
+sudo chmod go-r $HOME/.ssh/id_rsa
+sudo chmod a-w $HOME/.ssh/id_rsa.pub
+sudo chmod go-r  $HOME/.ssh/id_rsa.pub
 
 ssh -Tn drupal7druml.test@free-6255.devcloud.hosting.acquia.com "echo 123"
 
