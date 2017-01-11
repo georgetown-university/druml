40161ff4b4b2877f561d2198005d5fca6bbcf068
Vadym Myrgorod <dealancer@gmail.com>
2017-01-11 13:37:00 -0500

    Test run of druml in test script

--- test/test.sh
+++ test/test.sh
@@ -2,14 +2,10 @@
 printf $SSH_KEY
 printf $SSH_KEY_PUB
 
-echo "!"
-
-cat $HOME/.ssh/id_rsa
-cat $HOME/.ssh/id_rsa.pub
-
-echo "!!"
-
 ssh -Tn drupal7druml.test@free-6255.devcloud.hosting.acquia.com "echo 123"
 
 cd test
+
+druml remote-drush --site=default dev "cc all"
+
 ../bats/bin/bats test.bats 
